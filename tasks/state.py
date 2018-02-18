75031f2403149f7faf3e41d8c80b94977c43e9b6
Arthur <ar-2@tut.by>
2018-02-18 15:57:20 +0200

    add state pattern

---
+++ tasks/state.py
@@ -0,0 +1,48 @@
+# Данный шаблон затрагивает концепцию конечных автоматов 
+# (finite state machines) (или "FSM")
+#
+# class Hero:
+#   def handleInput(button):
+#       if button == BUTTON_B:
+#           if !self.isJumping:
+#               self.isJumping = True
+#               self.yVelocity = JUMP_VELOCITY;
+#               setGraphics(IMAGE_JUMP)
+#
+# Очень не удобно писать всю логику управления в if-ах.
+# Поэтому применим шаблон Состояние
+
+
+class HeroState:
+    standing = StandState
+    jumping = JumpState
+
+    @classmethod
+    def handleInput(hero, input):
+        pass
+
+class StandState(HeroState):
+    @classmethod
+    def handleInput(hero, input):
+        if input == UP:
+           self.yVelocity = JUMP_VELOCITY;
+           setGraphics(IMAGE_JUMP)
+           return HeroState.jumping
+
+
+class JumpState(HeroState):
+    @classmethod
+    def handleInput(hero, input):
+        if input == DOWN:
+           self.yVelocity = STUN_VELOCITY;
+           setGraphics(IMAGE_STUN)
+           return HeroState.standing
+
+class Hero:
+    def __init__(self):
+        self.state  = HeroState.standing
+
+    def handleInput(self, input):
+        state = self.state.handleInput(self, input)
+        if state is not None:
+            self.state = state
